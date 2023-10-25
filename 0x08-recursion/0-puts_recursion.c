824c81fc27531d50efb1c9e23f2b9811de0ca4ec
Ndatimana Assa <arthur.ndatimana22@gmail.com>
2023-10-25 14:01:22 -0700

    chang

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,36 @@
+#include <unistd.h>
+
+/**
+   * _putchar - writes the character c to stdout
+    * @c: The character to print
+     *
+      * Return: On success 1.
+       * On error, -1 is returned, and errno is set appropriately.
+        */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
+
+
+Task 0
 #include "main.h"
 
 /**
-  * _print_rev_recursion - main function
+  * _puts_recursion - main function
   *
-  * @s: Function parameter
+  * @s: function parameter
   *
-  * Return: Voidu
+  * Return: Always 0.
   */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-	_print_rev_recursion(s + 1);
 	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
+	else
+	_putchar('\n');
 }
