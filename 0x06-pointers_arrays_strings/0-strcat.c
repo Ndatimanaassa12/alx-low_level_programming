2b2ca353264febce9a0d90191e081a414ee6509c
Ndatimanaassa12 <arthur.ndatimana22@gmail.com>
2023-12-22 06:54:44 -0800

    change made

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,25 +6,17 @@
  *
  * Return: void
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src){
+while (*dest != "\0")
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+	dest++;
 }
-
+while (src != "\0")
+{
+	*dest = src ;
+	dest ++;
+	src ++;
+}
+*dest = "/0";
+return dest ;
+}
